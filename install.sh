f618324cb7a3b81a5182bb9956334f4eee8db0c0
Loaki <loaki.dev@gmail.com>
2024-10-07 16:40:32 +0200

    update

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt install i3 -y
 sudo apt install kitty -y
 sudo apt install zsh -y
 sudo apt install rofi -y
+sudo apt install pavucontrol -y
 sudo apt install pulseaudio -y
 sudo apt install python3 -y
 sudo apt install python3-i3ipc -y
@@ -18,10 +19,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; ~/.fzf/install
-sudo mv ~/.fzf/bin/fzf /usr/local/bin/
-sudo mv ~/.local/bin/zoxide /usr/local/bin/
+sudo mv ~/.fzf/bin/fzf /usr/bin/
+sudo mv ~/.local/bin/zoxide /usr/bin/
 chmod +x ~/boot/dotfiles/.scripts/autotiling
-sudo mv ~/boot/dotfiles/.scripts/autotiling /usr/local/bin/
+sudo mv ~/boot/dotfiles/.scripts/autotiling /usr/bin/
 chmod +x ~/boot/dotfiles/.scripts/lock.sh
 
 sudo mv ~/.zshrc ~/.zshrc.bak

c5633112a9afe656ab8499bbaa3692c3581411a2
Loaki <loaki.dev@gmail.com>
2025-03-26 16:27:50 +0100

    startup

--- dotfiles/.config/i3/scripts/mount_google_drive.sh
+++ dotfiles/.config/i3/scripts/mount_google_drive.sh
@@ -4,6 +4,7 @@ COMMAND="google-drive-ocamlfuse"
 
 if command -v $COMMAND > /dev/null 2>&1; then
     mkdir -p ~/GoogleDrive
+    fusermount -uz ~/GoogleDrive
     ERR_MSG=$($COMMAND ~/GoogleDrive 2>&1 >/dev/null)
     if [ $? -ne 0 ]; then
         notify-send "Error running $COMMAND" "$ERR_MSG"
--- dotfiles/.config/i3/scripts/startup.sh
+++ dotfiles/.config/i3/scripts/startup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-i3-msg "focus output HDMI-1"
-i3-msg "workspace 9"
-i3-msg "exec kitty -e lazydocker"
-sleep 1
-i3-msg "exec element-desktop"
-sleep 1
-i3-msg "focus left"
-i3-msg "split v"
-i3-msg "exec kitty -e spotify_player"
-sleep 1
-i3-msg "focus right"
-i3-msg "resize grow width 10 px or 10 ppt"
+local display=""
+if xrandr | grep "HDMI-1 connected"; then
+    display="HDMI-1"
+    xrandr --output eDP-1 --primary --output HDMI-1 --above eDP-1
+elif xrandr | grep "DP-3 connected"; then
+    display="DP-3"
+    xrandr --output DP-3 --primary --output DP-3 --left-of eDP-1
+fi
 
 i3-msg "focus output eDP-1"
 i3-msg "workspace 1"
@@ -23,3 +19,19 @@ i3-msg "exec kitty"
 sleep 1
 i3-msg "workspace 8"
 i3-msg "exec keepassxc"
+sleep 1
+
+if [ "$display" != "" ]; then
+    i3-msg "focus output $display"
+    i3-msg "workspace 9"
+    i3-msg "exec kitty -e lazydocker"
+    sleep 1
+    i3-msg "exec element-desktop"
+    sleep 1
+    i3-msg "focus left"
+    i3-msg "split v"
+    i3-msg "exec kitty -e spotify_player"
+    sleep 1
+    i3-msg "focus right"
+    i3-msg "resize grow width 10 px or 10 ppt"
+fi

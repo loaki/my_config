58766fd4c15ca3fc1ca44d45a5f841fb4259fd47
Loaki <loaki.dev@gmail.com>
2025-05-23 10:30:39 +0200

    web search

---
+++ dotfiles/.config/scripts/web_search.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+dir="$HOME/.config/rofi/launchers/type-1"
+theme='style-1'
+
+declare -A URLS
+
+URLS=(
+  ["google"]="https://www.google.com/search?q="
+  ["github"]="https://github.com/search?q="
+  ["stackoverflow"]="http://stackoverflow.com/search?q="
+  ["searchcode"]="https://searchcode.com/?q="
+  ["youtube"]="https://www.youtube.com/results?search_query="
+)
+
+# List for rofi
+gen_list() {
+    for i in "${!URLS[@]}"
+    do
+      echo "$i"
+    done
+}
+
+main() {
+  # Pass the list to rofi
+  platform=$( (gen_list) | rofi -dmenu -matching fuzzy -no-custom -location 0 -p "Platform: " -theme ~/.config/rofi/menu.rasi)
+
+  if [[ -n "$platform" ]]; then
+    query=$( (echo ) | rofi  -dmenu -matching fuzzy -location 0 -p "Search: " -theme ~/.config/rofi/menu.rasi)
+
+    if [[ -n "$query" ]]; then
+      url=${URLS[$platform]}$query
+      xdg-open "$url"
+    else
+      exit
+    fi
+
+  else
+    exit
+  fi
+}
+
+main
+
+exit 0
